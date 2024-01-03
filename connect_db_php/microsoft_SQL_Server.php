447cceccfd7be9a55e2652b046d2f830717fc084
Sometri OENG <67381317+sometri@users.noreply.github.com>
2024-01-03 17:32:14 +0700

    Add files via upload

--- connect_db_php/microsoft_SQL_Server.php
+++ connect_db_php/microsoft_SQL_Server.php
@@ -1,2 +1,22 @@
-1. Using PDO with SQL Server Driver for PHP:
-First, you need to ensure that you have the SQL Server Driver for PHP installed. Then you can use the following PHP code to connect to a Microsoft SQL Server database:
+<!-- 
+    1. Using PDO with SQL Server Driver for PHP:
+    First, you need to ensure that you have the SQL Server Driver for PHP installed. Then you can use the following PHP code to connect to a Microsoft SQL Server database: 
+-->
+<?php
+    $serverName = "localhost";
+    $connectionOptions = array(
+        "Database" => "your_database_name",
+        "Uid" => "your_username",
+        "PWD" => "your_password"
+    );
+
+    // Establishes the connection
+    $conn = new PDO("sqlsrv:Server=$serverName;Database={$connectionOptions['Database']}", $connectionOptions['Uid'], $connectionOptions['PWD']);
+
+    if ($conn) {
+        echo "Connected successfully";
+    } else {
+        echo "Connection failed";
+    }
+?>
+
